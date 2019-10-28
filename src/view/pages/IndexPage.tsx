ba0a1ae7935614cd063c642f5ef952be6f3376bf
zsh2401 <zsh2401@163.com>
2019-10-28 14:58:52 +0800

    change name to sz-rat

--- src/view/pages/IndexPage.tsx
+++ src/view/pages/IndexPage.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 export default class IndexPage extends React.Component{
     render(){
-        return <div><h1>Zsh's React Application Template</h1></div>
+        return <div><h1>Seymour Zhang's React Application Template</h1></div>
     }
 }
