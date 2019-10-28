bfaa76b864329115790d6f0be9c56184342d2a3f
zsh2401 <zsh2401@163.com>
2019-10-28 15:06:05 +0800

    add more function to common libs

--- src/common/debug-mx/index.ts
+++ src/common/debug-mx/index.ts
@@ -1,7 +1,20 @@
 const DEV_STR = "development"
+const PROD_STR = "production"
 function isDev():boolean{
-    return process.env.NODE_ENV === DEV_STR;
+    return process.env.NODE_ENV !== PROD_STR;
 }
-export default{
-    IS_DEV : isDev()
+function getVersion():string{
+    return require("../../../package.json").version;
+}
+function getPkgName():string{
+    return require("../../../package.json").name;
+}
+function getDescription():string{
+    return require("../../../package.json").description;
+}
+export default {
+    IS_DEV : isDev(),
+    VERSION_NAME : getVersion(),
+    PKG_NAME : getPkgName(),
+    DESC: getDescription(),
 }
