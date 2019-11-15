82ad1b71ef6f504957420a1c6ef0eee98b51d71a
zsh2401 <zsh2401@163.com>
2019-11-15 16:37:28 +0800

    update version

--- src/view/pages/IndexPage/index.tsx
+++ src/view/pages/IndexPage/index.tsx
@@ -6,7 +6,9 @@ export default function IndexPage(){
       <div className="row">
         <div className="col-md-6">
           <SZRatInfo/>
+          <br/>
         </div>
+        
         <div className="col-md-6">
           <div className="d-flex h-100 w-100 flex-column justify-content-center">
             <BilibiliVideo className="mr-auto ml-auto w-100" source="//player.bilibili.com/player.html?aid=67214844&cid=116546077&page=1"/>
