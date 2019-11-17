e8123ad951d646c4cba06b4577ddb47b50fabe40
zsh2401 <zsh2401@163.com>
2019-11-18 02:22:38 +0800

    remove some useless server code

--- scripts/server/hash/index.ts
+++ scripts/server/hash/index.ts
@@ -3,8 +3,6 @@ import fsPromise from '../fs-promise';
 import path from 'path'
 
 export default async function (ROOT_DIR: string, HOST: string, PORT: number) {
-    const CACHED_BODY_404 = await get404Body(ROOT_DIR);
-
     const app = express();
 
     app.use('/', express.static(ROOT_DIR));
@@ -15,7 +13,4 @@ export default async function (ROOT_DIR: string, HOST: string, PORT: number) {
     app.listen(PORT, HOST, () => {
         console.log(`[Hash Sever] started at ${PORT}`);
     });
-}
-async function get404Body(ROOT_DIR: string): Promise<Buffer> {
-    return await fsPromise.readFile(path.resolve(ROOT_DIR, "404.html"));
 }
