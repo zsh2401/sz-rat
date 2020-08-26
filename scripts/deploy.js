a7e10b1e9a3f85bbe4fed72e383dadddf9393d49
zsh2401 <zsh2401@163.com>
2020-08-26 13:33:07 +0800

    update dependencies

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,9 +1,10 @@
-const REPO = "https://github.com/zsh2401/sz-rat.git";
-const BRANCH = "gh-pages";
+// const REPO = "https://github.com/zsh2401/sz-rat.git";
+// const BRANCH = "gh-pages";
 
-const ghpages = require('gh-pages')
-const path = require('path');
-ghpages.publish(path.resolve(__dirname,'../dist'),{
-    branch:BRANCH,
-    repo: REPO
-});
+// const ghpages = require('gh-pages')
+// const path = require('path');
+// ghpages.publish(path.resolve(__dirname,'../dist'),{
+//     branch:BRANCH,
+//     repo: REPO
+// });
+console.log("Deploy logic");
